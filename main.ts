f7069af39397fb801798d00971e7da45a0536adc
hourfrozen <hourfrozen4@gmail.com>
2025-04-09 15:42:39 -0400

    commit wss

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 // @deno-types="npm:@types/express@4.17.15"
 import express from "npm:express";
-const port = 8000; //https
+
+const port = 8080; //https
 const app = express();
 import api from "./api.ts";
 
@@ -10,6 +11,22 @@ app.get("/", (_req, res) => {
   // res.sendFile(`../pages/landing.html`);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+Deno.serve((req) => {
+  if (req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 501 });
+  }
+  const { socket, response } = Deno.upgradeWebSocket(req);
+  socket.addEventListener("open", () => {
+    console.log("a client connected!");
+  });
+  socket.addEventListener("message", (event) => {
+    if (event.data === "ping") {
+      socket.send("pong");
+    }
+  });
+  return response;
 });
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+}); 
